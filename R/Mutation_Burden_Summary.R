bd92cb4f1ec1de65277b7a5644a8d2be5b8f1fd1
aobermayer4 <aobermayer4@gmail.com>
2024-01-22 15:24:21 -0500

    Add additional HRD columns

--- R/Mutation_Burden_Summary.R
+++ R/Mutation_Burden_Summary.R
@@ -10,7 +10,7 @@ Aneuploidy_File <- "J:\\Proj_IO_RIG_NOVA_Grant\\Harmonized_Softlinks\\work\\Alys
 
 VCF_Exome_Kit_File <- "~/R/Projects/Orien_Data_Processing/Data/Orien_VCF_Kit_Type.txt"
 
-Output_File <- "J:\\Proj_IO_RIG_NOVA_Grant\\Harmonized_Softlinks\\work\\Alyssa_Work\\WES\\Orien_IO_Rig_VAF_Summary_20240116_v2.txt"
+Output_File <- "J:\\Proj_IO_RIG_NOVA_Grant\\Harmonized_Softlinks\\work\\Alyssa_Work\\WES\\Orien_IO_Rig_VAF_Summary_20240116_v3.txt"
 
 
 aneu <- as.data.frame(read_delim(Aneuploidy_File,delim = '\t', col_names = T))
@@ -26,9 +26,9 @@ mutation_cols <- c("Variant_InFrame","Variant_OutOfFrame","Variant_MISSENSE","Va
 header <- c("WES","VCF_Sample_Name","VCF_Exome_Kit",
             "Variant_InFrame","Variant_InFrame_Norm","Variant_OutOfFrame","Variant_OutOfFrame_Norm",
             "Variant_MISSENSE","Variant_MISSENSE_Norm","Variant_NONSENSE","Variant_NONSENSE_Norm","Variant_SILENT","Variant_SILENT_Norm",
-            "dN.ds","dN.ds_GreaterThan_3","Aneuploidy_Score","HRD_Score",
+            "dN.ds","dN.ds_GreaterThan_3","Aneuploidy_Score","HRD_scarHRD","Telomeric.AI_scarHRD","LST_scarHRD","HRD.sum_scarHRD",
             "Total_Mutation_Count","Total_Mutation_Count_Norm","Total_Protein_Impacting_Mutation_Count","Total_Protein_Impacting_Mutation_Count_Norm")
-write(header,file = Output_File, append = T, sep = '\t', ncolumns = 21)
+write(header,file = Output_File, append = T, sep = '\t', ncolumns = 24)
 
 
 for (file in VCF_Anno_files) {
@@ -60,7 +60,10 @@ for (file in VCF_Anno_files) {
   dN.ds <- (Variant_MISSENSE+Variant_NONSENSE)/Variant_SILENT
   dN.ds_GreaterThan_3 <- ifelse(dN.ds > 3,TRUE,FALSE)
   Aneuploidy_Score <- aneu[which(aneu$WES == WES),"seg_Level_CNV_Aneuploidy"]
-  HRD_Score <- aneu[which(aneu$WES == WES),"HRD_scarHRD"]
+  HRD_scarHRD <- aneu[which(aneu$WES == WES),"HRD_scarHRD"]
+  Telomeric.AI_scarHRD <- aneu[which(aneu$WES == WES),"Telomeric.AI_scarHRD"]
+  LST_scarHRD <- aneu[which(aneu$WES == WES),"LST_scarHRD"]
+  HRD.sum_scarHRD <- aneu[which(aneu$WES == WES),"HRD.sum_scarHRD"]
   
   Total_Mutation_Count <- nrow(df[which(df[,"VAF"] != 0),])
   Total_Protein_Impacting_Mutation_Count <- Variant_InFrame+Variant_OutOfFrame
@@ -70,9 +73,9 @@ for (file in VCF_Anno_files) {
   Mutation_Burden_Summary <- c(WES,VCF_Sample_Name,VCF_Exome_Kit,
                                Variant_InFrame,Variant_InFrame_Norm,Variant_OutOfFrame,Variant_OutOfFrame_Norm,
                                Variant_MISSENSE,Variant_MISSENSE_Norm,Variant_NONSENSE,Variant_NONSENSE_Norm,Variant_SILENT,Variant_SILENT_Norm,
-                               dN.ds,dN.ds_GreaterThan_3,Aneuploidy_Score,HRD_Score,
+                               dN.ds,dN.ds_GreaterThan_3,Aneuploidy_Score,HRD_scarHRD,Telomeric.AI_scarHRD,LST_scarHRD,HRD.sum_scarHRD,
                                Total_Mutation_Count,Total_Mutation_Count_Norm,Total_Protein_Impacting_Mutation_Count,Total_Protein_Impacting_Mutation_Count_Norm)
-  write(Mutation_Burden_Summary,file = Output_File, append = T, sep = '\t', ncolumns = 21)
+  write(Mutation_Burden_Summary,file = Output_File, append = T, sep = '\t', ncolumns = 24)
   
 
   
